37e5bbcea7254055672874c76d864d76b87af21a
Dhruv <dhruv261dhiman@gmail.com>
2026-01-24 17:07:05 +0530

    remove unnecessary imports from teardown

--- src/fixtures/global-teardown.ts
+++ src/fixtures/global-teardown.ts
@@ -1,12 +1,8 @@
-import { Page , FullConfig} from "@playwright/test"
-import { linkSync } from "node:fs";
-import path from "node:path";
-
 import fs from "fs";
 const feature_gen_loc = '.features-gen';
 const storage_loc = 'storageState.json';
 
-const globalTeardown = async (config: FullConfig) => {
+const globalTeardown = async () => {
     if (fs.existsSync(feature_gen_loc)) {
         fs.rmSync(feature_gen_loc, { recursive: true, force: true });
     }
