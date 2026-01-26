a6de4806e483952b7c7f242539a2d23904e2f23b
Dhruv <dhruv261dhiman@gmail.com>
2026-01-26 17:30:55 +0530

    comment global setup steps, no need for the current project

--- playwright.config.js
+++ playwright.config.js
@@ -37,8 +37,8 @@ export default defineConfig({
   headless: false,
   use: {
     baseURL: "https://www.saucedemo.com/",
-    storageState: "storageState.json",
+    // storageState: "storageState.json",
   },
-  globalSetup: require.resolve("./src/fixtures/global-setup.ts"),
+  // globalSetup: require.resolve("./src/fixtures/global-setup.ts"),
   globalTeardown: require.resolve("./src/fixtures/global-teardown.ts"),
 });
